5be5c2019f588eaa116c9cbab3261ede7e4ae88b
Ryan Andrew <ryan.andrew88@ku.edu>
2023-10-11 19:04:35 -0500

    Commit for updated login page

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -1,21 +1,36 @@
 import React from "react";
 import { useAuth } from "../../util/auth/AuthProvider";
 import { useNavigate } from "react-router-dom";
+import "./Login.css"; // Import your CSS file
 
 export default function Login() {
   let auth = useAuth();
   let navigate = useNavigate();
+
+  const handleLogin = () => {
+    auth.signIn(() => {
+      navigate("/dashboard", { replace: true });
+    });
+  };
+
   return (
-    <>
-      <button
-        onClick={() =>
-          auth.signIn(() => {
-            navigate("/dashboard", { replace: true });
-          })
-        }
-      >
-        login
-      </button>
-    </>
+    <div className="login-container">
+      <div className="login-card">
+        <h1>Social Insights</h1>
+        <form>
+          <label>
+            Username:
+            <input type="text" placeholder="Enter your username" />
+          </label>
+          <label>
+            Password:
+            <input type="password" placeholder="Enter your password" />
+          </label>
+          <button type="button" onClick={handleLogin}>
+            Login
+          </button>
+        </form>
+      </div>
+    </div>
   );
 }
