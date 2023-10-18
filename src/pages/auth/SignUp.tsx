e2e033e8c79c9dd144fffde1a6184c1023383a7c
Ryan Andrew <ryan.andrew88@ku.edu>
2023-10-18 15:02:05 -0500

    Sign up page shell

--- src/pages/auth/SignUp.tsx
+++ src/pages/auth/SignUp.tsx
@@ -1,5 +1,48 @@
 import React from "react";
+import { useAuth } from "../../util/auth/AuthProvider";
+import { useNavigate } from "react-router-dom";
+import "./SignUp.css";
 
 export default function SignUp() {
-  return <>sign up</>;
+  let auth = useAuth();
+  let navigate = useNavigate();
+
+  const handleSignUp = () => {
+    auth.signIn(() => {
+      navigate("/dashboard", { replace: true });
+    });
+  };
+
+  return (
+    <div className="login-container">
+      <div className="login-card">
+        <h1>Sign Up</h1>
+        <form>
+          <label> 
+            First Name:
+            <input type="text" placeholder="First Name"></input>
+          </label>
+          <label> 
+            Last Name:
+            <input type="text" placeholder="Last Name"></input>
+          </label>
+          <label> 
+            Email:
+            <input type="text" placeholder="Example@example.com"></input>
+          </label>
+          <label>
+            Username:
+            <input type="text" placeholder="Enter a username" />
+          </label>
+          <label>
+            Password:
+            <input type="password" placeholder="Enter a password" />
+          </label>
+          <button type="button" onClick={handleSignUp}>
+            Sign up
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
