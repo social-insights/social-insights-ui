a1f164b94ac94c2e7f6c17b3e3fb3686a160467c
djlynn03 <djlynn03@gmail.com>
2023-10-27 01:05:48 -0500

    connected one widget with instagram api

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,86 +1,138 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Col, Container, Row } from "reactstrap";
 import InstagramLogo from "../../images/instagram-logo.webp";
 import FacebookLogo from "../../images/facebook.webp";
 
 import "./Dashboard.css";
+import { InstagramUserProvider, Profile, getInstagramUserById, useInstagramUser } from "../../util/accounts";
+import { supabase } from "../../supabase/supabase";
+import { useAuth } from "../../util/auth/AuthProvider";
 
 // TODO: Link actual follower amounts, change fonts
 
-
 export default function Dashboard() {
-  return (
-    <Container className="vh-100 p-4" fluid>
-      <Row className="h-100">
-        <Col
-          md={4}
-          className="h-100 d-flex flex-column gap-4 justify-content-start"
-        >
-          <Container className="h-25 foreground raised round-1">
-            <p className="mb-1" style={{}}>Social Media Followers</p>
-            <Row className="h-10">
-              <Col md={6} className="d-flex flex-column align-items-center">
-                <img
-                  alt="instagram"
-                  src={InstagramLogo}
-                  style={{
-                    height: '25%',
-                    width: '25%',
-                    margin: '2% 0 0 0',
-                  }}
-                />
-                <h2 className="mt-2">1,234,567</h2> 
-                <p style={{fontSize: '11px'}}>Followers</p>
 
-              </Col>
-              <Col md={6} className="d-flex flex-column align-items-center">
-                <img
-                  alt="facebook"
-                  src={FacebookLogo}
-                  style={{
-                    height: '25%',
-                    width: '25%',
-                    margin: '2% 0 0 0',
-                  }}
-                />
-                <h2 className="mt-2">674,438</h2> 
-                <p style={{fontSize: '11px'}}>Followers</p>
-              </Col>
-            </Row>
-          </Container>
-          <Container className="h-25 foreground raised round-1">
-            weekly change in followers/likes
-          </Container>
-          <Container className="h-50 foreground raised round-1">
-            key metrics last n days (clicks, likes, shares, etc.)
-          </Container>
-        </Col>
-        <Col
-          md={5}
-          className="h-100 d-flex flex-column gap-4 justify-content-start"
-        >
-          <Container className="h-50 foreground raised round-1">
-            engagements and impressions over time
-          </Container>
-          <Container className="h-50 foreground raised round-1">
-            recent post performance
-          </Container>
-        </Col>
-        <Col
-          md={3}
-          className="h-100 d-flex flex-column gap-4 justify-content-start"
-        >
-          <Container className="h-50 foreground raised round-1">
-            mentions
-          </Container>
-          <Container className="h-25 foreground raised round-1">
-            viewer demographics
-          </Container>
-          <Container className="h-25 foreground raised round-1">
-            other
-          </Container>
-        </Col>
-      </Row>
-    </Container>
+  const [user, setUser] = React.useState<Profile>();
+  const auth = useAuth();
+
+  useEffect(() => {
+    if (!user) {
+      supabase
+        .from("Organizations")
+        .select("instagram_id")
+        .eq("org_id", auth.org?.org_id)
+        .maybeSingle()
+        .then((data) => {
+          if (data.data && data.data.instagram_id) {
+            getInstagramUserById(
+              data.data.instagram_id,
+              auth.org?.org_id!
+            ).then((data) => {
+              setUser(data);
+              console.log(data);
+            });
+          }
+        });
+    }
+  }, [auth.org?.org_id, user]);
+
+  function nFormatter(num: number, digits: number) {
+    const lookup = [
+      { value: 1, symbol: "" },
+      { value: 1e3, symbol: "k" },
+      { value: 1e6, symbol: "M" },
+      { value: 1e9, symbol: "G" },
+      { value: 1e12, symbol: "T" },
+      { value: 1e15, symbol: "P" },
+      { value: 1e18, symbol: "E" }
+    ];
+    const rx = /\.0+$|(\.[0-9]*[1-9])0+$/;
+    var item = lookup.slice().reverse().find(function(item) {
+      return num >= item.value;
+    });
+    return item ? (num / item.value).toFixed(digits).replace(rx, "$1") + item.symbol : "0";
+  }
+  
+  return (
+    <InstagramUserProvider>
+      <Container className="vh-100 p-4" fluid>
+        <Row className="h-100">
+          <Col
+            md={4}
+            className="h-100 d-flex flex-column gap-4 justify-content-start"
+          >
+            <Container className="h-25 foreground raised round-1">
+              {/* <p className="mb-1" style={{}}>Social Media Followers</p> */}
+              <Row className="h-100 justify-content-center">
+                <Col
+                  md={6}
+                  className="d-flex flex-column align-items-center justify-content-center"
+                >
+                  <img
+                    alt="instagram"
+                    src={InstagramLogo}
+                    style={{
+                      height: 36,
+                      width: 36,
+                      margin: "2% 0 0 0",
+                    }}
+                  />
+                  <aside>@{user?.username}</aside>
+                  <h4>{user?.followers && nFormatter(user?.followers!, 2)}</h4>
+                  <p style={{ fontSize: "11px" }}>Followers</p>
+                </Col>
+                {/* <Col
+                  md={6}
+                  className="d-flex flex-column align-items-center justify-content-center"
+                >
+                  <img
+                    alt="facebook"
+                    src={FacebookLogo}
+                    style={{
+                      height: 36,
+                      width: 36,
+                      margin: "2% 0 0 0",
+                    }}
+                  />
+                  <h2 className="mt-2">674,438</h2>
+                  <p style={{ fontSize: "11px" }}>Followers</p>
+                </Col> */}
+              </Row>
+            </Container>
+            <Container className="h-25 foreground raised round-1">
+              weekly change in followers/likes
+            </Container>
+            <Container className="h-50 foreground raised round-1">
+              key metrics last n days (clicks, likes, shares, etc.)
+            </Container>
+          </Col>
+          <Col
+            md={5}
+            className="h-100 d-flex flex-column gap-4 justify-content-start"
+          >
+            <Container className="h-50 foreground raised round-1">
+              engagements and impressions over time
+            </Container>
+            <Container className="h-50 foreground raised round-1">
+              recent post performance
+            </Container>
+          </Col>
+          <Col
+            md={3}
+            className="h-100 d-flex flex-column gap-4 justify-content-start"
+          >
+            <Container className="h-50 foreground raised round-1">
+              mentions
+            </Container>
+            <Container className="h-25 foreground raised round-1">
+              viewer demographics
+            </Container>
+            <Container className="h-25 foreground raised round-1">
+              other
+            </Container>
+          </Col>
+        </Row>
+      </Container>
+    </InstagramUserProvider>
   );
 }
--- src/util/accounts.tsx
+++ src/util/accounts.tsx
@@ -1,4 +1,6 @@
 import { supabase } from "../supabase/supabase";
+import React, { useContext, useEffect } from "react";
+import { useAuth } from "./auth/AuthProvider";
 
 export type Profile = {
   username?: string;
@@ -13,7 +15,7 @@ export type Profile = {
   org_id?: number;
 };
 
-const STALE_TIMEOUT = 30;
+const STALE_TIMEOUT = 60;
 
 export function getInstagramUserByUsername(username: string, org_id: number) {
   // check supabase for the data first. if it exists and is not stale, return it
@@ -72,7 +74,7 @@ export function getInstagramUserById(id: string, org_id: number) {
         Date.parse(cacheData.data.last_updated) >
           Date.now() - STALE_TIMEOUT * 60 * 1000
       ) {
-        return cacheData;
+        return cacheData.data;
       } else {
         return fetch(
           `http://127.0.0.1:5001/social-insights-66e0d/us-central1/get_user/?id=${id}`
@@ -111,7 +113,10 @@ export function updateInstagramAccount(account: Profile) {
         last_updated: new Date().toISOString(),
       })
       .eq("account_id", account.id)
-      .eq("org_id", account.org_id);
+      .eq("org_id", account.org_id)
+      .then((data) => {
+        console.log(data);
+      });
   } else if (account.username) {
     supabase
       .from("Accounts")
@@ -128,7 +133,10 @@ export function updateInstagramAccount(account: Profile) {
         last_updated: new Date().toISOString(),
       })
       .eq("username", account.username)
-      .eq("org_id", account.org_id);
+      .eq("org_id", account.org_id)
+      .then((data) => {
+        console.log(data);
+      });
   } else {
     console.log("No account id or username provided");
   }
@@ -147,4 +155,51 @@ export function insertInstagramAccount(account: Profile) {
     is_private: account.is_private,
     last_updated: new Date().toISOString(),
   });
+}
+
+interface InstagramUserContextType {
+  user: Profile | undefined;
+  setUser: any;
+}
+
+const InstagramUserContext = React.createContext<InstagramUserContextType>(
+  null!
+);
+
+export const useInstagramUser = () => useContext(InstagramUserContext);
+
+export function InstagramUserProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [user, setUser] = React.useState<Profile>();
+  const auth = useAuth();
+
+  useEffect(() => {
+    if (!user) {
+      supabase
+        .from("Organizations")
+        .select("instagram_id")
+        .eq("org_id", auth.org?.org_id)
+        .maybeSingle()
+        .then((data) => {
+          if (data.data && data.data.instagram_id) {
+            getInstagramUserById(
+              data.data.instagram_id,
+              auth.org?.org_id!
+            ).then((data) => {
+              setUser(data);
+              console.log(data);
+            });
+          }
+        });
+    }
+  }, [auth.org?.org_id, user]);
+
+  return (
+    <InstagramUserContext.Provider value={{ user, setUser }}>
+      {children}
+    </InstagramUserContext.Provider>
+  );
 }
